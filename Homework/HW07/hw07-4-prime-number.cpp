fd5f72e86ec068a7cc120de5acc1b67fe7a4fc48
natthakit111 <natthakit_ya66@live.ac.th>
2023-08-14 00:51:17 +0700

    update hw7-4

--- Homework/HW07/hw07-4-prime-number.cpp
+++ Homework/HW07/hw07-4-prime-number.cpp
@@ -14,3 +14,37 @@
     Output:
         97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2
 */
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main() {
+    int num;
+    printf("Enter number:\n");
+    scanf("%d", &num);
+
+    printf("Output:\n");
+    do {
+        if (isPrime(num)) {
+            printf("%d ", num);
+        }
+        num--;
+    } while (num >= 2);
+
+    printf("\n");
+
+    return 0;
+}
